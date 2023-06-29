ac54cee9b1b2b58cefe087feb5cadd07b1371c62
DESKTOP-DK7UO56\z230 <tbthanha19055@gmail.com>
2023-06-29 17:03:02 +0700

    - add training.bat

---
+++ training.bat
@@ -0,0 +1,2 @@
+python train.py --batch 8 --cfg cfg/training/yolov7.yaml --epochs 300 --data ./tomato/data.yaml --weights 'yolov7.pt' --device 0 --name yolov7-tomato --hyp data/hyp.scratch.p6.yaml --workers 2
+pause
